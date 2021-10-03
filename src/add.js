aaccb7f6d9fe0b8cfd23ed291231e07be0f2ead3
Bogdan-K-A <kozhoma_b@ukr.net>
2021-10-03 11:35:18 +0300

    goit-js-hw-11

---
+++ src/add.js
@@ -0,0 +1,42 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+]
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+const refs = {
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+  body: document.body,
+}
+
+let timerId = null
+let isActive = false
+
+function bgColorChange() {
+  refs.body.style.backgroundColor =
+    colors[randomIntegerFromInterval(0, colors.length - 1)]
+}
+
+function onStart(e) {
+  if (!isActive) {
+    timerId = setInterval(bgColorChange, 1000)
+    isActive = true
+  }
+  return
+}
+
+function onStop(e) {
+  clearInterval(timerId)
+  isActive = false
+}
+
+refs.startBtn.addEventListener('click', onStart)
+refs.stopBtn.addEventListener('click', onStop)
